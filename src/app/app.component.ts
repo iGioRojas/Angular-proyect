74c93510f888afd76ac2cc0c0f081a99f8d8bb80
Geovanny Rojas <geovanny_rl14@hotmail.com>
2021-05-01 18:26:10 -0500

    Uso de ngFor y introduccion a Pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'filtros';
+  nombreEmpresa = "Marketing Digital";
+  titulo = "paquetes";
+  fecha = new Date();
+  mes = "/Mes";
+  precio = 200;
+
+  paquetes = [
+    {
+      titulo: 'Básico', 
+      precio: 100,
+      mes: '/mes',
+      d1: 'Redes Sociales',
+      d2: 'Branding',
+      d3: 'Diseño de imagen',
+      d4: '-'
+    },
+    {
+      titulo: 'Recomendado', 
+      precio: 200,
+      mes: 'mes',
+      d1: 'Redes Sociales',
+      d2: 'Branding',
+      d3: 'Diseño de imagen',
+      d4: 'Video'
+    },
+    {
+      titulo: 'Profesional', 
+      precio: 300,
+      mes: 'mes',
+      d1: 'Redes Sociales',
+      d2: 'Branding',
+      d3: 'Imagen/Video',
+      d4: 'Community Manager'
+    }
+  ]
 }
